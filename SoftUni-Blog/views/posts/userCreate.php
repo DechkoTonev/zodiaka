cbec3010ef8f4c343e0fd2a06719cde6d7d26dd9
dshterev <dg.shterev@gmail.com>
2016-08-29 06:08:24 +0300

    Update userCreate.php

--- SoftUni-Blog/views/posts/userCreate.php
+++ SoftUni-Blog/views/posts/userCreate.php
@@ -2,11 +2,17 @@
 
 <h1><?= htmlspecialchars($this->title)?></h1>
 
-<form method="post">
-    <div>Title:</div>
-    <input type="text" name="post_title">
-    <div>Content:</div>
-    <textarea rows="10" name="post_content"></textarea>
-    <div><input type="submit" value="Create">
-         <a href="<?=APP_ROOT?>/">[CANCEL]</a></div>
+<form method="post" id="create-form">
+    <label for="create-title">Title:</label>
+    <input type="text" name="post_title" id="create-title">
+    </br>
+    <label for="create-content">Content:</label>
+    <textarea rows="10" name="post_content" id="create-content"></textarea>
+    
+    <div>
+         <input type="submit" value="Create">
+        </br>
+        </br>
+         <a href="<?=APP_ROOT?>/" id="cancel-hlink">[CANCEL]</a>
+    </div>
 </form>
